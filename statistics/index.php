6d8e6ffac5ba60074b88f6c3b678affe565e816c
System Administrator <root@Jonathans-MacBook-Pro.local>
2015-11-08 23:09:17 +0100

    Added favicon

--- statistics/index.php
+++ statistics/index.php
@@ -8,6 +8,8 @@ session_start();
     <meta charset="UTF-8">
     <title>Checkbox Trickery: To-Do List</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="shortcut icon" href="images/ffcgicon.ico">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style2.css">
     
